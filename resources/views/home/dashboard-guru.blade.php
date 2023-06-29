38ee72fc27adc746b7d79f9ab57b84301547b07d
fajarraden09 <fajarraden9019@gmail.com>
2023-06-29 18:41:23 +0700

    update ini

--- resources/views/home/dashboard-guru.blade.php
+++ resources/views/home/dashboard-guru.blade.php
@@ -2,29 +2,31 @@
 
 <!-- ======= BAGIAN PAGES ======= -->
   @section('main')
-      <!-- page -->
-      <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-      <ol class="breadcrumb">
-              <li class="breadcrumb-item"></li>
-              <li class="breadcrumb-item active">Dashboard</li>
-          </ol>
-      </nav>
-      </div>
-      <!-- End Page Title -->
+    <main id="main" class="main">
+        <!-- page -->
+        <div class="pagetitle">
+          <h1>Dashboard</h1>
+          <nav>
+          <ol class="breadcrumb">
+                  <li class="breadcrumb-item"></li>
+                  <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+          </nav>
+        </div>
+        <!-- End Page Title -->
 
-    <section class="section dashboard">
-      <div class="row">
+      <section class="section dashboard">
+        <div class="row">
 
-          <!-- Left side columns -->
-          <div class="col-lg-8">
-          <div class="row">
-              <h1>hallo</h1>
-          </div>
-          <!-- End Right side columns -->
+            <!-- Left side columns -->
+            <div class="col-lg-8">
+            <div class="row">
+                <h1>hallo</h1>
+            </div>
+            <!-- End Right side columns -->
 
-      </div>
-    </section>
+        </div>
+      </section>
+    </main>
   @endsection
 <!-- End PAGES -->
