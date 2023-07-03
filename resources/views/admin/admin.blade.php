3707798638b770e82b5091eb471e0f325f1281cc
samagomenne@gmail.com <samagomenne@gmail.com>
2023-07-04 01:50:37 +0700

    adnub

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -92,25 +92,6 @@
       </li> -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-            <i class="bi bi-person"></i>
-            <span>Register Siswa dan Guru</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse dropdown-custom" data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="/register-siswa">
-              <i class="bi bi-circle"></i><span>Register Siswa</span>
-            </a>
-          </li>
-          <li>
-            <a href="/register-guru">
-              <i class="bi bi-circle"></i><span>Register Guru</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i>
             <span>Data Siswa dan Guru</span><i class="bi bi-chevron-down ms-auto"></i>
