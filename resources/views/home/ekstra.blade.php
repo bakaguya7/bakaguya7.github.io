0f3db4b081febcae010740c358cb446d20b85d8e
fajarraden09 <fajarraden9019@gmail.com>
2023-07-13 01:17:19 +0700

    coba

--- resources/views/home/ekstra.blade.php
+++ resources/views/home/ekstra.blade.php
@@ -2,16 +2,86 @@
 
 @section('isi-halaman')
     <div class="home">
-        <div class="container p-5">
-            <div class="card text-center" style="width: 100%;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="ekskul">
+            <div class="container p-5">
+                @if($Data->count() > 0)
+                <div class="card">
+                    <div class="card-item " >
+                        <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/kerja_sama/astra.jpg" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                            <h5 class="card-title">{{ $DataGuru->namalengkap }}</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-up"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="card-item " >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="img/kerja_sama/gameloft.jpg" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-up"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-item " >
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/kerja_sama/gmedia.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-up"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="card-item " >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/kerja_sama/shopee.jpg" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-up"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-item " >
+                    <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="img/kerja_sama/shopee.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-up"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
                 </div>
             </div>
 
-
-
+            </div>
         </div>
     </div>
 @endsection
